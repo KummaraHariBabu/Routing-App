f84c9e2d1eae6b0c61bcb71eae4188463a4d26ca
KummaraHariBabu <kummaraharibabu8@gmail.com>
2024-10-04 08:54:14 +0530

    add scripts for deploy and add iframe on courses component

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,23 +1,48 @@
-import React from "react";
-import courses from "../data/data";
+import React, { useState } from "react";
+import courses from "../data/data"; // Assuming this has {name, img, text, link}
+
 const Courses = () => {
+  const [selectedCourse, setSelectedCourse] = useState(null);
+
+  const handleClick = (course) => {
+    setSelectedCourse(course);
+  };
+
   return (
-    <div className="courseWrapper">
-      <h1>OUR COURSES</h1>
-      <div className="iconWrapper">
-        {courses.map((course,index) => {
-          const {  name, img, text } = course;
-          return (
-            <div className="card" key={index}>
-              <img src={img} alt="img" />
-              <div>
-                <h5>{name}</h5>
-                <p>{text}</p>
+    <div className="sectionWrapper">
+      {selectedCourse ? (
+        <div className="courseDetail">
+          <h1>{selectedCourse.name}</h1>
+          <iframe
+            src={selectedCourse.link}
+            title={selectedCourse.name}
+            style={{ width: "100%", height: "500px", border: "none" }}
+          />
+          <button onClick={() => setSelectedCourse(null)} className="btnWrapper">
+            Back to Courses
+          </button>
+        </div>
+      ) : (
+        <div className="courseWrapper">
+          <h1>OUR COURSES</h1>
+          <div className="iconWrapper">
+            {courses.map((course, index) => (
+              <div className="card" key={index}>
+                <img
+                  src={course.img}
+                  alt={course.name}
+                  onClick={() => handleClick(course)}
+                  style={{ cursor: "pointer" }} // Adds cursor pointer to indicate clickable
+                />
+                <div>
+                  <h5>{course.name}</h5>
+                  <p>{course.text}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/data/data.js
+++ src/data/data.js
@@ -4,19 +4,23 @@ const data = [
     name: "HTML",
     text: "Welcome to Our HTML Course",
     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/512px-HTML5_Badge.svg.PNG",
+    link:"https://www.javatpoint.com/html-tutorial",
   },
   {
     id: 1,
     name: "CSS",
     text: "Welcome to Our CSS Course",
     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/512px-CSS3_logo.svg.PNG",
+    link:"https://www.javatpoint.com/css-tutorial",
   },
   {
     id: 1,
     name: "JAVASCRIPT",
     text: "Welcome to Our JAVASCRIPT Course",
-    img:"https://www.seekpng.com/png/full/80-803501_javascript-logo-logo-de-java-script-png.png"
+    img:"https://www.seekpng.com/png/full/80-803501_javascript-logo-logo-de-java-script-png.png",
+    link:"https://www.javatpoint.com/javascript-tutorial",
    },
+   
 ];
 
 export default data;
